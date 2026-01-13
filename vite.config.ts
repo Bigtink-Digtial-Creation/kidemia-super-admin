a990fea1f01039f29d4263c129c2c3a504530632
samuel kufre willie <samuelkufrewillie@gmail.com>
2026-01-13 05:19:02 +0100

    bugFix: fix import bugs 2

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,23 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
-import path from "path";
 import tsconfigPaths from "vite-tsconfig-paths";
+import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
   resolve: {
     alias: [{ find: "@", replacement: path.resolve(__dirname, ".", "src") }],
   },
-  plugins: [react(),tsconfigPaths(), tailwindcss()],
+  plugins: [react(), tsconfigPaths(), tailwindcss()],
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router'],
+        },
+      },
+    },
+  },
 });
