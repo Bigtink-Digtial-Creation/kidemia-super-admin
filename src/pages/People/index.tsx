9c4aa678a39c818925b73c4039972cfb2937a06c
samuel kufre willie <samuelkufrewillie@gmail.com>
2026-07-01 14:42:33 +0100

    fix the assessment full for max attempt submission

--- src/pages/People/index.tsx
+++ src/pages/People/index.tsx
@@ -37,9 +37,6 @@ export default function UserManagementPage() {
 
     const { data: stats, isFetching: isStatsFetching } = useUserStats();
 
-    console.log('users', stats);
-
-
     const { roles: availableRoles } = useAvailableRoles();
     const updateUser = useUpdateUser();
     const updateUserRole = useUpdateUserRole();
